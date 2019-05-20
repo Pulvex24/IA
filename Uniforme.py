ac277827fc1b9728a9d88d52c084ef0b2ae881eb
Pulvex24 <49303365+Pulvex24@users.noreply.github.com>
2019-05-20 00:06:32 -0500

    Update Uniforme.py

--- Uniforme.py
+++ Uniforme.py
@@ -2,7 +2,7 @@
 import numpy as target  
 import matplotlib.pyplot as GRF
 
-values = target.random.uniform(0, 32000, 50000)  
-print(values)
-GRF.hist(values, 25)  
+aleatorio = target.random.uniform(0, 32000, 50000)  
+print(aleatorio)
+GRF.hist(aleatorio, 25)  
 GRF.show() 
