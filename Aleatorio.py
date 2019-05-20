e8062752da05d118772276272809485efdf73cad
Pulvex24 <49303365+Pulvex24@users.noreply.github.com>
2019-05-20 01:12:07 -0500

    Update Aleatorio.py

--- Aleatorio.py
+++ Aleatorio.py
@@ -3,4 +3,6 @@ aleatorio = target.random.normal(0, 32000, 8000)
 print(aleatorio)
 import matplotlib.pyplot as GRF  
 GRF.hist(aleatorio, 100)  
+GRF.title('DISTRIBUCION GAUSSIANA')
+GRF.xlabel('VALORES')
 GRF.show() 
